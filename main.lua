bc1775da2b8502ec67e128bfbddd4786306a2365
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2022-09-16 16:52:04 -0300

    initial comm,it, basic state definitions

---
+++ main.lua
@@ -0,0 +1,34 @@
+local love = require "love"
+
+local game = {
+  state = {
+    menu = true,
+    paused = false,
+    running = false,
+    ended = false
+  }
+}
+
+local player = {
+  radius = 20,
+  x = 30,
+  y = 30
+}
+
+function love.load()
+  love.window.setTitle("Save the ball")
+end
+
+function love.update()
+  player.x, player.y = love.mouse.getPosition()
+end
+
+function love.draw()
+  if game.state["running"] then
+    love.graphics.circle("fill", player.x, player.y, player.radius)
+  end
+
+  if not game.state["running"] then
+    love.graphics.circle("fill", player.x, player.y, player.radius / 2)
+  end
+end
